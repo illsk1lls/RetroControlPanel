cd4f6be5bdaf0955eb6d770f661fab471a52f1be
illsk1lls <gene.rovinsky@gmail.com>
2023-08-27 19:48:16 -0400

    Update RetroControlPanel.cmd

--- RetroControlPanel.cmd
+++ RetroControlPanel.cmd
@@ -8,7 +8,7 @@ CD /D %~dp0 & IF NOT "%~f0" EQU "%ProgramData%\%~nx0" (COPY /Y "%~f0" "%ProgramD
 FOR /F "usebackq skip=2 tokens=3-4" %%i IN (`REG QUERY "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName 2^>nul`) DO IF NOT "%%i %%j"=="Windows 10" ECHO. & ECHO Unsupported system detected. & ECHO. & PAUSE & EXIT
 >nul 2>&1 POWERSHELL -nop -c "(Add-Type -PassThru 'using System;using System.Runtime.InteropServices;namespace CloseButtonToggle{internal static class WinAPI{[DllImport(\"kernel32.dll\")]internal static extern IntPtr GetConsoleWindow();[DllImport(\"user32.dll\")][return:MarshalAs(UnmanagedType.Bool)]internal static extern bool DeleteMenu(IntPtr hMenu,uint uPosition,uint uFlags);[DllImport(\"user32.dll\")][return:MarshalAs(UnmanagedType.Bool)]internal static extern bool DrawMenuBar(IntPtr hWnd);[DllImport(\"user32.dll\")]internal static extern IntPtr GetSystemMenu(IntPtr hWnd,[MarshalAs(UnmanagedType.Bool)]bool bRevert);const uint SC_CLOSE=0xf060;const uint MF_BYCOMMAND=0;internal static void ChangeCurrentState(bool state){IntPtr hMenu=GetSystemMenu(GetConsoleWindow(),state);DeleteMenu(hMenu,SC_CLOSE, MF_BYCOMMAND);DrawMenuBar(GetConsoleWindow());}}public static class Status{public static void Disable(){WinAPI.ChangeCurrentState(false);}}}')[-1]::Disable()"
 :START
-CLS & ECHO. & ECHO                   MAIN MENU                %OPT%& ECHO ============================================& ECHO.
+CLS & ECHO. & ECHO                   MAIN MENU                & ECHO ============================================& ECHO.
 ECHO   (1) Add TCP/IP Printer
 ECHO   (2) Explorer/Folder View Options
 ECHO   (3) Firewall Settings
@@ -29,7 +29,7 @@ IF %errorlevel% == 6 START "" Rundll32.exe shell32.dll,Control_RunDLL powercfg.c
 IF %errorlevel% == 7 START "" Rundll32.exe shell32.dll,Control_RunDLL Sysdm.cpl,,1
 IF %errorlevel% == 8 START "" Rundll32.exe keymgr.dll,KRShowKeyMgr
 IF %errorlevel% == 9 START "" Rundll32.exe shell32.dll,Control_RunDLL nusrmgr.cpl
-IF %errorlevel% == 10 (IF "%OPT%"=="B" (SET "OPT=") ELSE (SET "OPT=B"))
-IF %errorlevel% == 11 CLS & ECHO. & ECHO Performing Automated Initial Setup%OPT%... & BITSADMIN /transfer "InitialSetup" /download /priority FOREGROUND "https://raw.githubusercontent.com/illsk1lls/InitialSetup/main/no-powershell/InitialSetup.cmd" "%~dp0InitialSetup%OPT%.cmd">nul & START "" "%~dp0InitialSetup%OPT%.cmd"
+IF %errorlevel% == 10 (IF "%OPT%"=="B" (SET "OPT=" & COLOR) ELSE (SET "OPT=B" & COLOR 06))
+IF %errorlevel% == 11 CLS & ECHO. & ECHO Performing InitialSetup%OPT%... & IF EXIST "%~dp0InitialSetup*.cmd" DEL "%~dp0InitialSetup*.cmd" /F /Q & BITSADMIN /transfer "InitialSetup" /download /priority FOREGROUND "https://raw.githubusercontent.com/illsk1lls/InitialSetup/main/no-powershell/InitialSetup.cmd" "%~dp0InitialSetup%OPT%.cmd">nul & START "" "%~dp0InitialSetup%OPT%.cmd"
 IF %errorlevel% == 12 (GOTO) 2>nul & DEL "%~f0">nul & EXIT
 GOTO START
