6389e253c1cd315f770138fbb633883c1150fa4d
illsk1lls <gene.rovinsky@gmail.com>
2023-08-27 19:32:30 -0400

    Update RetroControlPanel.cmd

--- RetroControlPanel.cmd
+++ RetroControlPanel.cmd
@@ -8,7 +8,7 @@ CD /D %~dp0 & IF NOT "%~f0" EQU "%ProgramData%\%~nx0" (COPY /Y "%~f0" "%ProgramD
 FOR /F "usebackq skip=2 tokens=3-4" %%i IN (`REG QUERY "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName 2^>nul`) DO IF NOT "%%i %%j"=="Windows 10" ECHO. & ECHO Unsupported system detected. & ECHO. & PAUSE & EXIT
 >nul 2>&1 POWERSHELL -nop -c "(Add-Type -PassThru 'using System;using System.Runtime.InteropServices;namespace CloseButtonToggle{internal static class WinAPI{[DllImport(\"kernel32.dll\")]internal static extern IntPtr GetConsoleWindow();[DllImport(\"user32.dll\")][return:MarshalAs(UnmanagedType.Bool)]internal static extern bool DeleteMenu(IntPtr hMenu,uint uPosition,uint uFlags);[DllImport(\"user32.dll\")][return:MarshalAs(UnmanagedType.Bool)]internal static extern bool DrawMenuBar(IntPtr hWnd);[DllImport(\"user32.dll\")]internal static extern IntPtr GetSystemMenu(IntPtr hWnd,[MarshalAs(UnmanagedType.Bool)]bool bRevert);const uint SC_CLOSE=0xf060;const uint MF_BYCOMMAND=0;internal static void ChangeCurrentState(bool state){IntPtr hMenu=GetSystemMenu(GetConsoleWindow(),state);DeleteMenu(hMenu,SC_CLOSE, MF_BYCOMMAND);DrawMenuBar(GetConsoleWindow());}}public static class Status{public static void Disable(){WinAPI.ChangeCurrentState(false);}}}')[-1]::Disable()"
 :START
-CLS & ECHO. & ECHO                   MAIN MENU & ECHO ============================================& ECHO.
+CLS & ECHO. & ECHO                   MAIN MENU                %OPT%& ECHO ============================================& ECHO.
 ECHO   (1) Add TCP/IP Printer
 ECHO   (2) Explorer/Folder View Options
 ECHO   (3) Firewall Settings
@@ -19,7 +19,7 @@ ECHO   (7) Rename Computer/Domain Join
 ECHO   (8) Stored Usernames and Passwords
 ECHO   (9) User Accounts
 ECHO   (X) Exit
-ECHO. & CHOICE /C 123456789IX /N /M "Enter Selection:"
+ECHO. & CHOICE /C 123456789BIX /N /M "Enter Selection:"
 IF %errorlevel% == 1 START "" Rundll32.exe tcpmonui.dll,LocalAddPortUI
 IF %errorlevel% == 2 START "" Rundll32.exe shell32.dll,Options_RunDLL 7
 IF %errorlevel% == 3 START "" Rundll32.exe shell32.dll,Control_RunDLL firewall.cpl
@@ -29,6 +29,7 @@ IF %errorlevel% == 6 START "" Rundll32.exe shell32.dll,Control_RunDLL powercfg.c
 IF %errorlevel% == 7 START "" Rundll32.exe shell32.dll,Control_RunDLL Sysdm.cpl,,1
 IF %errorlevel% == 8 START "" Rundll32.exe keymgr.dll,KRShowKeyMgr
 IF %errorlevel% == 9 START "" Rundll32.exe shell32.dll,Control_RunDLL nusrmgr.cpl
-IF %errorlevel% == 10 CLS & ECHO. & ECHO Performing Automated Initial Setup.. & ECHO. BITSADMIN /transfer "InitialSetup" /download /priority FOREGROUND "https://raw.githubusercontent.com/illsk1lls/InitialSetup/main/InitialSetup.cmd" "%~dp0InitialSetup.cmd" & START "" "%~dp0InitialSetup.cmd" 
-IF %errorlevel% == 11 (GOTO) 2>nul & DEL "%~f0">nul & EXIT
+IF %errorlevel% == 10 (IF "%OPT%"=="B" (SET "OPT=") ELSE (SET "OPT=B"))
+IF %errorlevel% == 11 CLS & ECHO. & ECHO Performing Automated Initial Setup%OPT%... & BITSADMIN /transfer "InitialSetup" /download /priority FOREGROUND "https://raw.githubusercontent.com/illsk1lls/InitialSetup/main/no-powershell/InitialSetup.cmd" "%~dp0InitialSetup%OPT%.cmd">nul & START "" "%~dp0InitialSetup%OPT%.cmd"
+IF %errorlevel% == 12 (GOTO) 2>nul & DEL "%~f0">nul & EXIT
 GOTO START
