dc07d6265b24f46153ca5429446788c4c3556818
illsk1lls <gene.rovinsky@gmail.com>
2023-08-27 21:11:02 -0400

    Update RetroControlPanel.cmd

--- RetroControlPanel.cmd
+++ RetroControlPanel.cmd
@@ -13,17 +13,17 @@ FOR /F "usebackq skip=2 tokens=3-4" %%i IN (`REG QUERY "HKLM\SOFTWARE\Microsoft\
 >nul 2>&1 POWERSHELL -nop -c "(Add-Type -PassThru 'using System;using System.Runtime.InteropServices;namespace CloseButtonToggle{internal static class WinAPI{[DllImport(\"kernel32.dll\")]internal static extern IntPtr GetConsoleWindow();[DllImport(\"user32.dll\")][return:MarshalAs(UnmanagedType.Bool)]internal static extern bool DeleteMenu(IntPtr hMenu,uint uPosition,uint uFlags);[DllImport(\"user32.dll\")][return:MarshalAs(UnmanagedType.Bool)]internal static extern bool DrawMenuBar(IntPtr hWnd);[DllImport(\"user32.dll\")]internal static extern IntPtr GetSystemMenu(IntPtr hWnd,[MarshalAs(UnmanagedType.Bool)]bool bRevert);const uint SC_CLOSE=0xf060;const uint MF_BYCOMMAND=0;internal static void ChangeCurrentState(bool state){IntPtr hMenu=GetSystemMenu(GetConsoleWindow(),state);DeleteMenu(hMenu,SC_CLOSE, MF_BYCOMMAND);DrawMenuBar(GetConsoleWindow());}}public static class Status{public static void Disable(){WinAPI.ChangeCurrentState(false);}}}')[-1]::Disable()"
 FOR /F "tokens=1,2 delims=#" %%a IN ('"PROMPT #$H#$E# & ECHO ON & FOR %%b IN (1) DO REM"') DO SET ESC=%%b
 :START
-CLS & ECHO. & ECHO                   %ESC%[1mMAIN MENU%ESC%[0m                & ECHO %ESC%[31m============================================%ESC%[0m & ECHO.
-ECHO   %ESC%[1m(%ESC%[0m%ESC%[4m1%ESC%[0m%ESC%[1m)%ESC%[0m Add TCP/IP Printer
-ECHO   %ESC%[1m(%ESC%[0m%ESC%[4m2%ESC%[0m%ESC%[1m)%ESC%[0m Explorer/Folder View Options
-ECHO   %ESC%[1m(%ESC%[0m%ESC%[4m3%ESC%[0m%ESC%[1m)%ESC%[0m Firewall Settings
-ECHO   %ESC%[1m(%ESC%[0m%ESC%[4m4%ESC%[0m%ESC%[1m)%ESC%[0m Map Network Drive
-ECHO   %ESC%[1m(%ESC%[0m%ESC%[4m5%ESC%[0m%ESC%[1m)%ESC%[0m Network Adapters
-ECHO   %ESC%[1m(%ESC%[0m%ESC%[4m6%ESC%[0m%ESC%[1m)%ESC%[0m Power Options
-ECHO   %ESC%[1m(%ESC%[0m%ESC%[4m7%ESC%[0m%ESC%[1m)%ESC%[0m Rename Computer/Domain Join
-ECHO   %ESC%[1m(%ESC%[0m%ESC%[4m8%ESC%[0m%ESC%[1m)%ESC%[0m Stored Usernames and Passwords
-ECHO   %ESC%[1m(%ESC%[0m%ESC%[4m9%ESC%[0m%ESC%[1m)%ESC%[0m User Accounts
-ECHO   %ESC%[1m(%ESC%[0m%ESC%[4mX%ESC%[0m%ESC%[1m)%ESC%[0m Exit
+CLS & ECHO. & ECHO                   %ESC%[1mMAIN MENU%ESC%[0m                & ECHO %ESC%[31m============================================%ESC%[0m& ECHO.
+ECHO   %ESC%[1m(%ESC%[0m%ESC%[31m1%ESC%[0m%ESC%[1m)%ESC%[0m Add TCP/IP Printer
+ECHO   %ESC%[1m(%ESC%[0m%ESC%[31m2%ESC%[0m%ESC%[1m)%ESC%[0m Explorer/Folder View Options
+ECHO   %ESC%[1m(%ESC%[0m%ESC%[31m3%ESC%[0m%ESC%[1m)%ESC%[0m Firewall Settings
+ECHO   %ESC%[1m(%ESC%[0m%ESC%[31m4%ESC%[0m%ESC%[1m)%ESC%[0m Map Network Drive
+ECHO   %ESC%[1m(%ESC%[0m%ESC%[31m5%ESC%[0m%ESC%[1m)%ESC%[0m Network Adapters
+ECHO   %ESC%[1m(%ESC%[0m%ESC%[31m6%ESC%[0m%ESC%[1m)%ESC%[0m Power Options
+ECHO   %ESC%[1m(%ESC%[0m%ESC%[31m7%ESC%[0m%ESC%[1m)%ESC%[0m Rename Computer/Domain Join
+ECHO   %ESC%[1m(%ESC%[0m%ESC%[31m8%ESC%[0m%ESC%[1m)%ESC%[0m Stored Usernames and Passwords
+ECHO   %ESC%[1m(%ESC%[0m%ESC%[31m9%ESC%[0m%ESC%[1m)%ESC%[0m User Accounts
+ECHO   %ESC%[1m(%ESC%[0m%ESC%[31mX%ESC%[0m%ESC%[1m)%ESC%[0m Exit
 ECHO. & CHOICE /C 123456789BIX /N /M "Enter Selection:"
 IF %errorlevel% == 1 START "" Rundll32.exe tcpmonui.dll,LocalAddPortUI
 IF %errorlevel% == 2 START "" Rundll32.exe shell32.dll,Options_RunDLL 7
